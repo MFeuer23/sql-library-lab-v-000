25e9e7964c07c2a90e8439f6224205f51e37302a
Mallory Feuer <mallory.feuer@gmail.com>
2018-03-14 19:21:52 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -2,10 +2,17 @@ CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
   author_id INTEGER,
+<<<<<<< HEAD
   subgenre_id INTEGER
   );
   
 CREATE TABLE subgenres (
+=======
+  sub_genre_id INTEGER
+  );
+  
+CREATE TABLE sub_genre (
+>>>>>>> 08035a1fbd72534681eeb4fb7150dc54dd8b242b
   id INTEGER PRIMARY KEY,
   name TEXT
   );
--- lib/update.sql
+++ lib/update.sql
@@ -1,3 +1,7 @@
 UPDATE characters
 SET species = 'Martian'
+<<<<<<< HEAD
 WHERE characters.id = 8;
+=======
+WHERE characters.id = 1;
+>>>>>>> 08035a1fbd72534681eeb4fb7150dc54dd8b242b
